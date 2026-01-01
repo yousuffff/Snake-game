ed9500a6b92835afd9831ca0bf86fc5c76da4d68
Mohd Yousuf <mdyousuf43201@gmail.com>
2026-01-01 16:20:03 +0530

    new edit

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const board = document.querySelector(".board");
-const blockHeight = 30;
-const blockWidth = 30;
-const col = Math.floor(board.clientWidth / blockWidth);
-const row = Math.floor(board.clientHeight / blockHeight);
+const blockHeight = 50;
+const blockWidth = 50;
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
 
-for (let i = 0; i < row * col; i++) {
-  const block = document.createElement("div");
-  block.classList.add("block");
-  board.appendChild(block);
+let blocks = [];
+let snake = [
+  { x: 5, y: 5 },
+  { x: 5, y: 6 },
+  { x: 5, y: 7 },
+];
+
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+    block.innerText = `${col},${row}`;
+    blocks[`${col}-${row}`] = block;
+  }
 }
+
+function renderSnake() {
+   snake.forEach((segment) => {
+    console.log(blocks[`${segment.x}-${segment.y}`].classList.add("snake"));
+    return 1
+  });
+}
+renderSnake();
